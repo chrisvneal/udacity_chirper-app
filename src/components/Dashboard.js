b25f41cccfa9d1cd481a0180c913071e9b0309f4
Chris Neal <chrisvneal@gmail.com>
2021-02-03 14:57:15 -1000

    Build Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,19 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Dashboard extends Component {
   render() {
+    console.log(this.props);
     return <div>Dashboard</div>;
   }
 }
+
+function mapStateToProps({ tweets }) {
+  return {
+    tweetIds: Object.keys(tweets).sort(
+      (a, b) => tweets[b].timestamp - tweets[a].timetsamp
+    ),
+  };
+}
+
+export default connect(mapStateToProps)(Dashboard);
