6d61d2a205f9aa3332296be56759a78544e9ef89
Chris Neal <chrisvneal@gmail.com>
2021-02-11 12:52:59 -1000

    Build out UI for New Tweet component

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -4,23 +4,21 @@ class NewTweet extends Component {
   state = {
     text: "",
   };
-
   handleChange = (e) => {
-    const text = e.target.useDebugValue(
-      this.setState(() => ({
-        text,
-      }))
-    );
-  };
+    const text = e.target.value;
 
+    this.setState(() => ({
+      text,
+    }));
+  };
   handleSubmit = (e) => {
     e.preventDefault();
 
     const { text } = this.state;
 
-    // todo: Add tweet to store
+    // todo: Add Tweet to Store
 
-    console.log("New tweet: ", text);
+    console.log("New Tweet: ", text);
 
     this.setState(() => ({
       text: "",
@@ -28,9 +26,16 @@ class NewTweet extends Component {
   };
   render() {
     const { text } = this.state;
+
+    {
+      /* Redirect to home view if submitted*/
+    }
+
+    const tweetLeft = 280 - text.length;
+
     return (
       <div>
-        <h3 className="center">Compose new tweet</h3>
+        <h3 className="center">Compose new Tweet</h3>
         <form className="new-tweet" onSubmit={this.handleSubmit}>
           <textarea
             placeholder="What's happening?"
@@ -39,6 +44,10 @@ class NewTweet extends Component {
             className="textarea"
             maxLength={280}
           />
+          {tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>}
+          <button className="btn" type="submit" disabled={text === ""}>
+            Submit
+          </button>
         </form>
       </div>
     );
