ea0cbbdbe9013e50c0bd6b40a2de5b051f6dd6f7
Chris Neal <chrisvneal@gmail.com>
2021-02-10 17:37:32 -1000

    Handle changes to New Tweet form

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,31 @@
 import React, { Component } from "react";
 
 class NewTweet extends Component {
+  state = {
+    text: "",
+  };
+
+  handleChange = (e) => {
+    const text = e.target.useDebugValue(
+      this.setState(() => ({
+        text,
+      }))
+    );
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { text } = this.state;
+
+    // todo: Add tweet to store
+
+    console.log("New tweet: ", text);
+
+    this.setState(() => ({
+      text: "",
+    }));
+  };
   render() {
     return <div>New Tweet</div>;
   }
